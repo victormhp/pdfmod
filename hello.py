68b8c6d28b44a53a31f6e92f79a6f1e68e5c9c1c
victormhp <victoralbertomhp@gmail.com>
2025-02-05 23:04:43 -0600

    Setup pypdf and typer

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from pdf-mod!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ pdfmod/__main__.py
@@ -0,0 +1,9 @@
+from pdfmod import cli, __app__
+
+
+def main():
+    cli.app(prog_name=__app__)
+
+
+if __name__ == "__main__":
+    main()
